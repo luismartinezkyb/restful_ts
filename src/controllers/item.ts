e6a8e6353d0238b459a532de12ebeb15381b0a99
luismartinezkyb <18030545@itcelaya.edu.mx>
2023-10-19 22:44:10 -0600

    (feat): Schemas con validacion

--- src/controllers/item.ts
+++ src/controllers/item.ts
@@ -1,28 +1,37 @@
 import { Request, Response } from "express";
 import { handleHttpError } from "../utils/errors.handle";
-import { Car } from "../interfaces/car.interface";
+
+import { insertCar, getCars, getCar } from "../services/item";
 
 const getItem = async(req:Request, res:Response)=>{
   try{
-    res.json({success:true})
+    const data = await getCar(req.params.id);
+    console.log("data",data)
+    if(!data) {
+      return handleHttpError(res, 'Car not found');
+    }
+    
+    res.json(data)
   }catch(e){
     
-    handleHttpError(res, "ERROR_GET_ITEM");
+    handleHttpError(res, "ERROR_GET_ITEM", e);
   }
 }
-const getItems = (req:Request, res:Response)=>{
+const getItems = async(req:Request, res:Response)=>{
   try{
-
+    const data = await getCars();
+    res.json(data);
   }catch(e){
     handleHttpError(res, 'Error_GET_ITEMS');
   }
 }
-const postItem = ({body}:Request, res:Response)=>{
+const postItem = async({body}:Request, res:Response)=>{
   try{
-    res.send(body)
-  }catch(e){
     
-    handleHttpError(res, "ERROR_postItem");
+    const responseItem = await insertCar(body);
+    res.json(responseItem)
+  }catch(e){
+    handleHttpError(res, "ERROR_postItem", e);
   }
 }
 const updateItem = (req:Request, res:Response)=>{
--- src/utils/errors.handle.ts
+++ src/utils/errors.handle.ts
@@ -1,6 +1,7 @@
 import { Response } from "express"
 
-export const handleHttpError = (res: Response, error:string, code:number = 403)=>{
+export const handleHttpError = (res: Response, error:string, errorRaw?:any,code:number = 403 )=>{
+  console.log(errorRaw)
   res.status(code)
   res.send({ error });
   return;
